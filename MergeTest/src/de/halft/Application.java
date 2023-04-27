2e384bf3581295a7acb720207bfc4bcce5fd369f
CreepyNiki <nlh352@gmail.com>
2023-04-27 17:19:40 +0200

    Bayrische Version

--- MergeTest/src/de/halft/Application.java
+++ MergeTest/src/de/halft/Application.java
@@ -2,7 +2,7 @@ package de.halft;
 
 public class Application {
 public static void main(String[] args) {
-	System.out.println("moin");
-	Sysout.println("seeya");
+	System.out.println("sevus");
+	Sysout.println("grieß gott");
 }
 }
