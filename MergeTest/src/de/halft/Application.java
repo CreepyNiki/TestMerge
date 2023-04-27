92a8748636e43dd65196e7dfb1ecf2aa0c939871
CreepyNiki <nlh352@gmail.com>
2023-04-27 17:05:00 +0200

    improved version

--- MergeTest/src/de/halft/Application.java
+++ MergeTest/src/de/halft/Application.java
@@ -2,7 +2,7 @@ package de.halft;
 
 public class Application {
 public static void main(String[] args) {
-	System.out.println("hallo tst!!");
-	
+	System.out.println("Lalala");
+	Sysout.println("test");
 }
 }
